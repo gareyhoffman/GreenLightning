a83900db5e13b6e5b77c792a3cb43e5421e1b3e2
David Giovannini <dave@Davids-MacBook-Pro.local>
2017-12-22 10:39:28 -0500

    api change

--- src/test/java/com/ociweb/gl/example/ProngStructExample.java
+++ src/test/java/com/ociweb/gl/example/ProngStructExample.java
@@ -2,11 +2,94 @@ package com.ociweb.gl.example;
 
 import com.ociweb.gl.api.*;
 import com.ociweb.gl.structure.GreenRational;
-import com.ociweb.gl.structure.GreenRationalExternalizable;
+import com.ociweb.gl.structure.GreenRationalStruct;
+import com.ociweb.pronghorn.structure.annotations.ProngProperty;
+import com.ociweb.pronghorn.structure.annotations.ProngStruct;
+
+@ProngStruct
+abstract class Big {
+    abstract byte getByte();
+    abstract void setByte(byte value);
+
+    abstract boolean getBoolean();
+    abstract void setBoolean(boolean value);
+
+    abstract char getChar();
+    abstract void setChar(char value);
+
+    abstract short getShort();
+    abstract void setShort(short value);
+
+    abstract int getInt();
+    abstract void setInt(int value);
+
+    abstract long getLong();
+    abstract void setLong(long value);
+
+    abstract float getFloat();
+    abstract void setFloat(float value);
+
+    abstract double getDouble();
+    abstract void setDouble(double value);
+
+    @ProngProperty(nullable=true)
+    abstract Byte getOByte();
+    @ProngProperty(nullable=true)
+    abstract void setOByte(Byte value);
+
+    @ProngProperty(nullable=true)
+    abstract Boolean getOBoolean();
+    @ProngProperty(nullable=true)
+    abstract void setOBoolean(Boolean value);
+
+    @ProngProperty(nullable=true)
+    abstract Character getOChar();
+    @ProngProperty(nullable=true)
+    abstract void setOChar(Character value);
+
+    @ProngProperty(nullable=true)
+    abstract Short getOShort();
+    @ProngProperty(nullable=true)
+    abstract void setOShort(Short value);
+
+    @ProngProperty(nullable=true)
+    abstract Integer getOInt();
+    @ProngProperty(nullable=true)
+    abstract void setOInt(Integer value);
+
+    @ProngProperty(nullable=true)
+    abstract Long getOLong();
+    @ProngProperty(nullable=true)
+    abstract void setOLong(Long value);
+
+    @ProngProperty(nullable=true)
+    abstract Float getOFloat();
+    abstract void setOFloat(Float value);
+
+    abstract Double getODouble();
+    @ProngProperty(nullable=true)
+    abstract void setODouble(Double value);
+
+    abstract GreenRational getRational1();
+    abstract void setRational1(GreenRational rational);
+
+    @ProngProperty(nullable=true)
+    abstract GreenRational getRational2();
+    @ProngProperty(nullable=true)
+    abstract void setRational2(GreenRational rational);
+
+    @ProngProperty(nullable=true)
+    abstract GreenRational getRational3();
+    abstract void setRational3(GreenRational rational);
+
+    abstract GreenRational getRational4();
+    @ProngProperty(nullable=true)
+    abstract void setRational4(GreenRational rational);
+}
 
 class ProngStructBehavior implements PubSubMethodListener, TimeListener {
     private final GreenCommandChannel cmd;
-    private final GreenRationalExternalizable writeData = new GreenRationalExternalizable();
+    private final GreenRational writeData = new GreenRationalStruct();
 
     ProngStructBehavior(GreenRuntime runtime) {
         cmd = runtime.newCommandChannel();
